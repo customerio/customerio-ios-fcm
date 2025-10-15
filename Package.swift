ba33925c319eaadb1e50af4d82f61a3381ede1cc
Mahmoud Elmorabea <mahmoud.elmorabea@gmail.com>
2025-10-15 14:12:16 +0400

    Change Firebase version range

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
          .package(url: "https://github.com/customerio/customerio-ios.git", branch: "test-wrapper"),
-         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "12.0.0"))
+         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", "8.7.0"..<"13.0.0")
     ],
     targets: [
         .target(
